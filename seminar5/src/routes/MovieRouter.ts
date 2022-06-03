007410621f250453f335e80b2c89cfc8aa1e712f
laalaa31 <jseoh4298@gmail.com>
2022-06-04 04:43:53 +0900

    [FEAT] 5차 세미나

---
+++ seminar5/src/routes/MovieRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { body } from "express-validator/check";
+import MovieController from "../controllers/MovieController";
+import auth from "../middlewares/auth";
+
+
+const router: Router = Router();
+
+router.post('/', [
+    body("title").notEmpty(),
+    body("director").notEmpty()
+], MovieController.createMovie);
+router.put('/:movieId', MovieController.updateMovie);
+router.get('/:movieId', MovieController.findMovieById);
+router.delete('/:movieId', MovieController.deleteMovie);
+
+router.post('/:movieId/comment', [
+    body('writer').notEmpty(),
+    body('comment').notEmpty()
+], MovieController.createMovieComment);
+router.put('/:movieId/comments/:commentId', [
+    body('comment').notEmpty()
+], auth, MovieController.updateMovieComment);
+
+export default router;
---
+++ seminar5/src/services/index.ts
@@ -0,0 +1,12 @@
+import UserService from "./UserService"
+import ReviewService from "./ReviewService"
+import MovieService from "./MovieService"
+
+//service index file
+//Controller에서 사용 가능
+export {
+    UserService,
+    ReviewService,
+    MovieService
+
+}
